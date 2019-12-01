b4771cfff8df7fa74139433a21b47536749a6a65
Gaurav Manda <grvmanda@gmail.com>
2019-12-01 08:42:24 -0600

    added code with no obstacles - doesnt work

--- test_dubins.m
+++ test_dubins.m
@@ -63,17 +63,7 @@ poses = interpolate(pathSegObj{1},0:dlen:len);
 figure;
 quiver(poses(:,1),poses(:,2),cos(poses(:,3)),sin(poses(:,3)),0.5)
 
-%% Stanley delta calculations
-
-delta_refST = zeros(1, length(poses));
-
-for i = 1:1:length(poses)-1
-    delta_refST(i) = deg2rad(lateralControllerStanley(poses(i+1, :), ...
-        poses(i, :), constVel, 'Wheelbase', L, 'PositionGain', 2.5));
-end
-
-% delta_const = deg2rad(lateralControllerStanley(poses(end, :), ...
-%         poses(1, :), constVel, 'Wheelbase', L, 'PositionGain', 2.5));
+%% delta reference calculation
 
 delta_ref = ones(1, length(poses))*0.02635;    
     
@@ -98,6 +88,7 @@ hold off;
 
 %% lane change maneuver
 
+%{
 dubConnObj = dubinsConnection;
 
 startPose = [0 0 0];
@@ -122,4 +113,199 @@ dlen = constVel*dt;
 poses = interpolate(pathSegObj{1},0:dlen:len);
 figure;
 quiver(poses(:,1),poses(:,2),cos(poses(:,3)),sin(poses(:,3)),0.5)
+%}
 
+%% test MPC without obstacles
+
+%% Model Setup
+Y_ref = poses';
+U_ref = [constVel*ones(1, length(poses));...
+    delta_ref];
+
+x = @(i) Y_ref(1, i);
+y = @(i) Y_ref(2, i);
+psi = @(i) Y_ref(3, i);
+
+u = @(i) U_ref(1, i);
+delta = @(i) U_ref(2, i);
+
+A_disc = @(i) [0, 0, (-u(i)*sin(psi(i))-b/L*u(i)*tan(delta(i))*cos(psi(i)));...
+    0, 0, (u(i)*cos(psi(i))-b/L*u(i)*tan(delta(i))*sin(psi(i)));...
+    0, 0, 0];
+  
+A = @(i) (eye(3) + dt*A_disc(i));
+
+B = @(i) dt*[(cos(psi(i))-b/L*tan(delta(i))*sin(psi(i))), ...
+    ((-b/L)*u(i)*(sec(delta(i)))^2*sin(psi(i)));...
+    (sin(psi(i))+b/L*tan(delta(i))*cos(psi(i))),...
+    (b/L*u(i)*(sec(delta(i)))^2*cos(psi(i)));...
+    (1/L*tan(delta(i))), (u(i)/L*(sec(delta(i)))^2)];
+
+%% MPC Setup
+
+nstates=3;
+ninputs=2;
+
+%input ranges (first row is longitudinal velocity, second row is wheel angle)
+input_range=[0,   5;...
+            -0.5,0.5];
+
+%11 timesteps for 3 states, 10 timesteps for 2 inputs
+npred=10;
+Ndec=(npred+1)*nstates+ninputs*npred;
+T = 0:dt:dt*(length(poses)-1);
+
+%final trajectory
+Y=NaN(3,length(T));
+
+%applied inputs
+U=NaN(2,length(T));
+
+%input from QP
+u_mpc=NaN(2,length(T));
+
+%error in states (actual-reference)
+eY=NaN(3,length(T));
+
+%set random initial condition
+eY0=[0;-105;0];
+Y(:,1)=eY0-Y_ref(:,1);
+
+for i=1:length(T)-1
+    %shorten prediction horizon if we are at the end of trajectory
+    npred_i=min([npred,length(T)-i]);
+    
+    %calculate error
+    eY(:,i)=Y(:,i)-Y_ref(:,i);
+
+    %generate equality constraints
+    [Aeq,beq]=eq_cons(i,A,B,eY(:,i),npred_i,nstates,ninputs);
+    
+    %generate boundary constraints
+    [Lb,Ub]=bound_cons(i,U_ref,npred_i,input_range,nstates,ninputs);
+    
+    %cost for states
+    Q=[1,1,0.5];
+    
+    %cost for inputs
+    R=[0.1,0.01];
+    
+    H=diag([repmat(Q,[1,npred_i+1]),repmat(R,[1,npred_i])]);
+    
+    f=zeros(nstates*(npred_i+1)+ninputs*npred_i,1);
+    
+    [x,fval] = quadprog(H,f,[],[],Aeq,beq,Lb,Ub);
+    
+    %get linearized input
+    u_mpc(:,i)=x(nstates*(npred_i+1)+1:nstates*(npred_i+1)+ninputs);
+    
+    %get input
+    U(:,i)=u_mpc(:,i)+U_ref(:,i);
+    
+    
+    %simulate model
+    [~,ztemp]=ode45(@(t,z)kinematic_bike_dynamics(t,z,U(:,i),0,b,L),[0 dt],Y(:,i));
+    
+    %store final state
+    Y(:,i+1)=ztemp(end,:)';
+end
+
+figure;
+subplot(3,1,1)
+plot(Y_ref(1,:),Y_ref(2,:))
+hold on
+xlabel('x [m]')
+ylabel('y [m]')
+subplot(3,1,2)
+plot(Y_ref(1,:),U_ref(1,:))
+hold on
+xlabel('x [m]')
+ylabel('u [m/s]')
+subplot(3,1,3)
+plot(Y_ref(1,:),U_ref(2,:))
+hold on
+xlabel('x [m]')
+ylabel('\delta_f [rad]')
+
+subplot(3,1,1)
+plot(Y(1,:),Y(2,:))
+subplot(3,1,2)
+plot(Y_ref(1,:),U(1,:))
+subplot(3,1,3)
+plot(Y_ref(1,:),U(2,:))
+
+
+
+
+%% functions for constraint generation and dynamics
+
+
+function [Aeq,beq]=eq_cons(initial_idx,A,B,x_initial,npred,nstates,ninputs)
+%build matrix for A_i*x_i+B_i*u_i-x_{i+1}=0
+%in the form Aeq*z=beq
+%initial_idx specifies the time index of initial condition from the reference trajectory 
+%A and B are function handles above
+
+%initial condition
+x_initial=x_initial(:);
+
+%size of decision variable and size of part holding states
+zsize=(npred+1)*nstates+npred*ninputs;
+xsize=(npred+1)*nstates;
+
+Aeq=zeros(xsize,zsize);
+Aeq(1:nstates,1:nstates)=eye(nstates); %initial condition 
+beq=zeros(xsize,1);
+beq(1:nstates)=x_initial;
+
+state_idxs=nstates+1:nstates:xsize;
+input_idxs=xsize+1:ninputs:zsize;
+
+for i=1:npred
+    %negative identity for i+1
+    Aeq(state_idxs(i):state_idxs(i)+nstates-1,state_idxs(i):state_idxs(i)+nstates-1)=-eye(nstates);
+    
+    %A matrix for i
+    Aeq(state_idxs(i):state_idxs(i)+nstates-1,state_idxs(i)-nstates:state_idxs(i)-1)=A(initial_idx+i-1);
+    
+    %B matrix for i
+    Aeq(state_idxs(i):state_idxs(i)+nstates-1,input_idxs(i):input_idxs(i)+ninputs-1)=B(initial_idx+i-1);
+end
+
+end
+
+function [Lb,Ub]=bound_cons(initial_idx,U_ref,npred,input_range,nstates,ninputs)
+%time_idx is the index along uref the initial condition is at
+xsize=(npred+1)*nstates;
+usize=npred*ninputs;
+
+Lb=[];
+Ub=[];
+for j=1:ninputs
+Lb=[Lb;input_range(j,1)-U_ref(j,initial_idx:initial_idx+npred-1)];
+Ub=[Ub;input_range(j,2)-U_ref(j,initial_idx:initial_idx+npred-1)];
+end
+
+Lb=reshape(Lb,[usize,1]);
+Ub=reshape(Ub,[usize,1]);
+
+Lb=[-Inf(xsize,1);Lb];
+Ub=[Inf(xsize,1);Ub];
+
+end
+
+function dzdt=kinematic_bike_dynamics(t,z,U_in,T,b,L)
+
+if length(T)<=1 || isempty(T) || size(U_in,2)==1
+    delta=U_in(2);
+    u=U_in(1);
+else
+    delta=interp1(T',U_in(2,:)',t,'previous');
+    u=interp1(T',U_in(1,:)',t,'previous');
+end
+
+dzdt=[u*cos(z(3))-b/L*u*tan(delta)*sin(z(3));...
+      u*sin(z(3))+b/L*u*tan(delta)*cos(z(3));...
+      u/L*tan(delta)];
+
+end
